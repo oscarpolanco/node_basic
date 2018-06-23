ad3c0ea758e6aac7ed50caa7cf03b67b307f63fc
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-23 17:33:38 -0400

    Clean up and use nodemon for the first time

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,22 +1,6 @@
 console.log('Staring app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
-
-var filteredArray = _.uniq(['O', 1, 'O', 1, 2, 3, 4]);
-console.log(filteredArray);
-// console.log(notes.add(1, 2));
-
-// let user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}!!! You are ${notes.age}.`, function(err) {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
