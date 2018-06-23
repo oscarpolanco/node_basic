c4504182b8806dd710914fd96740b2025aaf6068
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-23 16:45:17 -0400

    Require my own files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,15 @@
-console.log('Staring app.');
+console.log('Staring app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-let user = os.userInfo();
+console.log(notes.add(1, 2));
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!!!`, function(err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// let user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}!!! You are ${notes.age}.`, function(err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
