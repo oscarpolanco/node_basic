72dce320cfeb3472835594bf7f4706bf904915ce
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-06-23 17:13:57 -0400

    Use 3er party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,16 @@ console.log('Staring app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log(notes.add(1, 2));
+
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+var filteredArray = _.uniq(['O', 1, 'O', 1, 2, 3, 4]);
+console.log(filteredArray);
+// console.log(notes.add(1, 2));
 
 // let user = os.userInfo();
 //
