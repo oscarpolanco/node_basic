0fe78c682506c6f62643e8560181445adfab2de0
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-07 18:53:36 -0400

    Using yargs to simplify the inputs

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,22 +2,27 @@ console.log('Staring app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 // Array of arrguments of node
-console.log(process.argv);
-const command = process.argv[2];
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+// const command = process.argv[2];
+const command = argv._[0];
 console.log('Command: ', command);
 
 if (command === 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all note');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Fetching note');
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing note');
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized');
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,24 @@
 console.log('Starting notes.js');
 
-// console.log(module);
-// module.exports.age = 25;
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
-}
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Geting this note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing this note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
