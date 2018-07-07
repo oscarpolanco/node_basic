c4cda8a2c430aac0c227bd42d3a4e9de42166d3e
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-07 18:17:39 -0400

    Reciving arguments from the user

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,20 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+// Array of arrguments of node
+console.log(process.argv);
+const command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all note');
+} else if (command === 'read') {
+  console.log('Fetching note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
