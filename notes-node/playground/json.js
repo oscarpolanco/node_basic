5ac72e73a8e140c2cc9cddcef734ad743ac49805
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-10 11:21:37 -0400

    Write on a json file and read the object if that file

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -1,11 +1,16 @@
-var obj = {
-  name: 'Andrew'
-};
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+const fs = require('fs');
 
-var personString = '{"name": "Andrew","age": 25}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+// originalNoteString
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+// note
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
