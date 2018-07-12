c611c7c8bc0cc4959e3cbe4e5e405183e4b012a7
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-12 17:52:19 -0400

    Add the read all notes functionallity and clean up

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,3 @@
-console.log('Staring app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -7,13 +5,9 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-// Array of arrguments of node
-console.log('Process', process.argv);
-console.log('Yargs', argv);
 
 // const command = process.argv[2];
 const command = argv._[0];
-console.log('Command: ', command);
 
 if (command === 'add') {
 var note = notes.addNote(argv.title, argv.body);
@@ -24,7 +18,9 @@ if (note) {
   console.log("Note title already taken");
 }
 } else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
   var note = notes.getNote(argv.title);
   if (note) {
