88d0c1095ceae3a93220ec78b1aceba363ca8d77
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-14 16:33:59 -0400

    Add conditions to handle request errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -19,7 +19,13 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to Goggle serves.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
+  }
 });
