02eb607e78dc7edcc303c31f60cdb2f4c4945b35
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-16 16:21:33 -0400

    Separe and refactor the geoCodeAddress function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
-const request = require('request');
 const yargs = require ('yargs');
 
+const geocode = require('./geocode/geocode');
 const argv = yargs
   .options({
     a: {
@@ -14,18 +14,10 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Goggle serves.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(result, undefined, 2));
   }
 });
