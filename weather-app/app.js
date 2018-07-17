62433e8a9607af5ff126843b47e752a6005e1a76
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-17 11:40:29 -0400

    Add a fixed request using forecast.io

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,36 @@
-const yargs = require ('yargs');
-
-const geocode = require('./geocode/geocode');
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-})
-.help()
-.alias('help', 'h')
-.argv;
-
-geocode.geocodeAddress(argv.address, (errorMessage, result) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+// const yargs = require ('yargs');
+//
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(result, undefined, 2));
+//   }
+// });
+const request = require('request');
+request({
+  url: 'https://api.darksky.net/forecast/4cca74c34a0de7d0d80714857b768351/39.9444071,-75.1631718',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(result, undefined, 2));
+    console.log('Unable to fetch weather');
   }
 });
+// 4cca74c34a0de7d0d80714857b768351
+// https://api.darksky.net/forecast/4cca74c34a0de7d0d80714857b768351/39.9444071,-75.1631718
