a8206983077a660ffa44de11353d00c52d355237
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-19 11:48:23 -0400

    render a json using express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express!!</h1>');
+  res.send({
+    name: 'Andrew',
+    likes: [
+      'Biking',
+      'Cities'
+    ]
+  })
 });
 app.listen(3000);
