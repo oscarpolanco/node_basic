da17b3052fdaf4667eb5d8db7116127f8ea699a9
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-07-19 17:19:49 -0400

    Add multiple routes

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -11,4 +11,16 @@ app.get('/', (req, res) => {
     ]
   })
 });
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+//  /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
 app.listen(3000);
