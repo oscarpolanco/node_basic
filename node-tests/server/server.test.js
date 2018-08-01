c3144819c69f0441979fad3fdfeae96dd5a1d5a6
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-08-01 18:07:57 -0400

    Add describe for the test

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,16 +3,42 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Hello world!!!')
-    .end(done);
-});
+// server
+  //  GET /
+    // some test case
+  //  GET / users
+    // some test case
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+      .get('/')
+      .expect(200)
+      .expect('Hello world!!!')
+      .end(done);
+    });
+  });
 
-it('should return a page not found response', (done) => {
-  request(app)
+  describe('GET /users', () => {
+    // Make a new test
+    //  assert 200
+    // Assert that you exist in user array
+    it('should return an array of users', (done) => {
+      request(app)
+      .get('/users')
+      .expect((res) => {
+        expect(res.body)
+        .toInclude({
+          name: 'Test',
+          age: 20
+        });
+      })
+      .end(done);
+    });
+  });
+
+  it('should return a page not found response', (done) => {
+    request(app)
     .get('/error')
     .expect(404)
     .expect((res) => {
@@ -21,20 +47,5 @@ it('should return a page not found response', (done) => {
       });
     })
     .end(done);
-});
-
-// Make a new test
-//  assert 200
-// Assert that you exist in user array
-it('should return an array of users', (done) => {
-  request(app)
-    .get('/users')
-    .expect((res) => {
-      expect(res.body)
-        .toInclude({
-          name: 'Test',
-          age: 20
-        });
-    })
-    .end(done);
+  });
 });
