b84ab35b6cfe6a2ab58aee330b9aaf123aaebd60
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-08-11 13:25:49 -0400

    Add a count query and an user query

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -6,15 +6,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   }
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
-
-  db.collection('Todos').find({
-    _id: new ObjectID('5b65e943c72f9f4db0dc53db')
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to fetch todos', err);
   });
 
+  db.collection('Users').find({name: 'Test'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
   // client.close();
 });
