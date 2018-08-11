2345d8810dceb2f5df761dea32e3a817a90f1415
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-08-11 12:07:45 -0400

    Query a document by id

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -7,7 +7,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+  db.collection('Todos').find({
+    _id: new ObjectID('5b65e943c72f9f4db0dc53db')
+  }).toArray().then((docs) => {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
