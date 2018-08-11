b4de3a5ce652b25fbebef5148e597790c1efb205
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-08-11 12:01:44 -0400

    Use a query with a parameter to find a specific document

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -7,7 +7,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  db.collection('Todos').find().toArray().then((docs) => {
+  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
