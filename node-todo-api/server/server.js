117f4524f285a9ca4c37c7a17a9e69ac36cd7ae2
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-08-27 19:24:58 -0400

    Add GET /todos/:id end point

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
@@ -26,6 +27,32 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET/todos/1234
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  // valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    // 404 - send back empty body
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    // if not todo - send back 404 with empty body
+    if (!todo) {
+      return res.status(404).send();
+    }
+    // success
+    // if todo - send it back
+    res.send({todo});
+  }).catch((e) => {
+    // error
+    // 400 - send empty body back
+    res.status(400).send();
   });
 });
 
