6afdd24c781b62afa383fa2032479f0d1007ec59
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-09-07 18:49:52 -0400

    Setup separate test and development envs

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODN_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODN_URI, { useNewUrlParser: true });
 
 module.exports = {mongoose};
