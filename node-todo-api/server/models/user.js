04b8180591c4623304b92a827235e0d0be42e019
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-09-08 14:42:13 -0400

    Add the validator module, update the user model and add a POST/users end point

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const User = mongoose.model('User', {
   email: {
     type: String,
     require: true,
     trim: true,
-    minlenght: 1
-  }
+    minlenght: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{Value} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlenght: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
 
 module.exports = {User};
