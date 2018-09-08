f82c4c83f4e5b5876990da729726ba8e1d11eefd
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-09-08 15:22:08 -0400

    Add an example using jwt

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,24 +1,35 @@
 const {SHA256} = require('crypto-js');
-
-const message = 'I am user number 3';
-const hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 const data = {
-  id: 4
+  id: 10
 };
 
-const token = {
-  data,
-  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
+const token = jwt.sign(data, '123abc');
+console.log(token);
 
-const resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+const decoded = jwt.verify(token, '123abc');
+console.log('decoded:', decoded);
 
-if (resultHash === token.hash) {
-  console.log('Data was not changed');
-} else {
-  console.log('Data was changed. Do not trust!!!');
-}
+// const message = 'I am user number 3';
+// const hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// const data = {
+//   id: 4
+// };
+//
+// const token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+//
+// const resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+//
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was changed. Do not trust!!!');
+// }
