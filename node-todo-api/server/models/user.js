2c4f4fb09cba7b9ddb6243fb22b83898d79f7bfe
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-09-11 18:54:19 -0400

    Update the POST /user route and add the token method

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     require: true,
@@ -29,5 +31,25 @@ const User = mongoose.model('User', {
     }
   }]
 });
+
+UserSchema.methods.toJSON = function () {
+  const user = this;
+  const UserObject = user.toObject();
+
+  return _.pick(UserObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -115,8 +115,10 @@ app.patch('/todos/:id', (req, res) => {
    const body = _.pick(req.body, ['email', 'password']);
    const user = new User(body);
 
-   user.save().then((user) => {
-     res.send(user);
+   user.save().then(() => {
+     return user.generateAuthToken();
+   }).then((token) => {
+     res.header('x-auth', token).send(user);
    }).catch((e) => {
      res.status(400).send(e);
    });
