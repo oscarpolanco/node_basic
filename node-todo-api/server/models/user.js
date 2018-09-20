becc1fd65ddcbcde8e1dd5361bfd46b6b1032cce
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-09-20 19:03:51 -0400

    Add the POST/user/login route and the findByCredential function on the user model

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -71,6 +71,28 @@ UserSchema.statics.findByToken = function (token) {
   })
 };
 
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (!res) {
+          reject();
+        }
+
+        resolve(user);
+      })
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   const user = this;
 
