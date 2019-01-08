fca15b5fb9a14f91c45dffaddce12a7ffcde022f
Oscar Polanco <itt.oscarpolanco@gmail.com>
2019-01-08 16:01:22 -0400

    Fix script test watch on the package json, add the socekt for the location message and add a generate location message function with test

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -23,6 +23,16 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  const li = jQuery('<li></li>');
+  const a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -18,4 +18,20 @@ describe('generateMessage', () => {
       const type = typeof(message.createAt);
       expect(type).toBe("number");
     });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Admin';
+    const url = "https://www.google.com/maps?q=1,1";
+    const message = generateLocationMessage(from, 1, 1);
+
+    expect(message).toMatchObject({
+      from,
+      url
+    });
+
+    const type = typeof(message.createAt);
+    expect(type).toBe("number");
+  });
 });
