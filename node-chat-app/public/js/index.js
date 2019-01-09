265faeb87899fc63b63f43e40c3cbca7ab5e49d9
Oscar Polanco <itt.oscarpolanco@gmail.com>
2019-01-09 11:12:30 -0400

    Add css file for the app, add classes and change the index structure, add logic to the send location button and some clean up

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -36,11 +36,12 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  const messgeTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messgeTextbox.val()
   }, function () {
-
+    messgeTextbox.val('');
   });
 });
 
@@ -51,12 +52,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not suppored by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
