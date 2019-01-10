6e6b8945d4f301e7186c51fb574b17494d29dbdc
Oscar Polanco <itt.oscarpolanco@gmail.com>
2019-01-10 19:33:02 -0400

    Add the getUser, getListUser and removeUser function to the user class and test

--- node-chat-app/server/utils/user.test.js
+++ node-chat-app/server/utils/user.test.js
@@ -3,6 +3,24 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+  let users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Mike',
+      room: 'Node Course'
+    }, {
+      id: '2',
+      name: 'Jen',
+      room: 'React Course'
+    }, {
+      id: '3',
+      name: 'Julie',
+      room: 'Node Course'
+    }]
+  });
   it('should add new user', () => {
     const users = new Users();
     const user = {
@@ -12,5 +30,42 @@ describe('Users', () => {
     };
     const reUser = users.addUser(user.id, user.name, user.room);
     expect(users.users).toEqual([user]);
+  });
+
+  it('should return names for node course', () => {
+    const userList = users.getUserList('Node Course');
+
+    expect(userList).toEqual(['Mike', 'Julie']);
+  });
+
+  it('should return names for react course', () => {
+    const userList = users.getUserList('React Course');
+
+    expect(userList).toEqual(['Jen']);
+  });
+
+  it('should remove a user', () => {
+    const newUsers = users.removeUser('1');
+
+    expect(newUsers.length).toBe(2);
+  });
+
+  it('should not remove user', () => {
+    const newUsers = users.removeUser('11');
+
+    expect(newUsers.length).toBe(3);
+  });
+
+  it('should find user', () => {
+    const user = users.getUser('1');
+
+    expect(user).toMatchObject(users.users[0]);
+  });
+
+  it('should not find user', () => {
+    const user = users.getUser('22');
+
+    expect(user).toBeFalsy();
+
   });
 });
--- node-chat-app/server/utils/users.js
+++ node-chat-app/server/utils/users.js
@@ -7,6 +7,21 @@ class Users {
     this.users.push(user);
     return user;
   }
+  removeUser (id) {
+    return  this.users.filter((user) => user.id !== id);
+  }
+
+  getUser (id) {
+    return  this.users.filter((user) => user.id === id)[0];
+
+  }
+
+  getUserList (room) {
+    const users = this.users.filter((user) => user.room === room);
+    const namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
 }
 
 module.exports = {Users};
